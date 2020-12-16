b4b9812dd074952da5607eabb7cea99745e26c15
unknown <yjyoon.dev@gmail.com>
2020-12-17 02:53:52 +0900

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,51 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.CompoundButton
+import android.widget.RadioGroup
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var checkBoxLister = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+        when(buttonView.id){
+            R.id.checkApple -> Log.d("check","사과를")
+            R.id.checkBanana -> Log.d("check","바나나를")
+            R.id.checkOrange -> Log.d("check","오렌지를")
+        }
+        if(isChecked) Log.d("check","선택하였습니다")
+        else Log.d("check","해제하였습니다")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSay.setOnClickListener {
-            textSay.text = getString(R.string.helloKotlin)
+        editText.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                Log.d("EditText","현재 입력된 값=${s.toString()}")
+            }
+        })
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId){
+                R.id.radioRed -> {Log.d("radio","빨강선택")}
+                R.id.radioBlue -> {Log.d("radio","파랑선택")}
+                R.id.radioGreen -> {Log.d("radio","초록선택")}
+            }
         }
+
+        checkApple.setOnCheckedChangeListener(checkBoxLister)
+        checkBanana.setOnCheckedChangeListener(checkBoxLister)
+        checkOrange.setOnCheckedChangeListener(checkBoxLister)
     }
 }
