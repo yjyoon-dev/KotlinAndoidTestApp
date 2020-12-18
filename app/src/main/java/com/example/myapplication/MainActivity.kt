1e23fa72981f0dcd286a5b6ddb929294c9b4fe5c
unknown <yjyoon.dev@gmail.com>
2020-12-19 02:31:23 +0900

    test widgets

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,13 +5,16 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.CompoundButton
 import android.widget.RadioGroup
+import android.widget.SeekBar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
-    var checkBoxLister = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
+    var checkBoxListner = CompoundButton.OnCheckedChangeListener { buttonView, isChecked ->
         when(buttonView.id){
             R.id.checkApple -> Log.d("check","사과를")
             R.id.checkBanana -> Log.d("check","바나나를")
@@ -24,6 +27,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        thread(start=true){
+            while(true){
+                Thread.sleep(100);
+                runOnUiThread {
+                    var percentText = progressText.text.toString()
+                    progressText.text = (percentText.slice(IntRange(0,percentText.length-2)).toInt()+1).toString() + "%"
+                    if(progressText.text == "100%") progressLayout.visibility = View.GONE
+                }
+            }
+        }
+
+        thread(start=true){
+            while(true){
+                kotlinImage.rotationX -=1
+                Thread.sleep(10)
+                if(kotlinImage.rotationX==-360f)
+                    kotlinImage.rotationX=360f
+            }
+        }
 
         editText.addTextChangedListener(object: TextWatcher{
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
@@ -45,8 +68,31 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        checkApple.setOnCheckedChangeListener(checkBoxLister)
-        checkBanana.setOnCheckedChangeListener(checkBoxLister)
-        checkOrange.setOnCheckedChangeListener(checkBoxLister)
+        checkApple.setOnCheckedChangeListener(checkBoxListner)
+        checkBanana.setOnCheckedChangeListener(checkBoxListner)
+        checkOrange.setOnCheckedChangeListener(checkBoxListner)
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                seekText.text = "$progress"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                //TODO("Not yet implemented")
+            }
+        })
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            rateText.text = "$rating"
+        }
     }
+
+    fun showProgress(show: Boolean) {
+        if(show) progressLayout.visibility = View.VISIBLE
+        else progressLayout.visibility = View.GONE
+    }
+
 }
